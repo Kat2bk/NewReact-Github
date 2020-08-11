1fac60dae5dd8649de4ac5321bce8b9991ec1dc4
unknown <Katherine2bk@gmail.com>
2020-08-11 13:35:22 -0700

    changed UserCard.js to a class component

--- github-app/src/App.js
+++ github-app/src/App.js
@@ -14,7 +14,6 @@ class App extends React.Component {
   componentDidMount() {
     axios.get("https://api.github.com/users/Kat2bk")
     .then(response => {
-      console.log("This is the data", response.data)
       this.setState({ user: response.data})
     })
     .catch(error => {
@@ -28,7 +27,7 @@ class App extends React.Component {
       <div className="App-container">
       <h1>GitHub Users</h1>
       <h2>Let's Git Crazy!</h2>
-      <UserCard />
+      <UserCard userData={this.state.user}/>
       </div>
     )
   }
--- github-app/src/components/UserCard.js
+++ github-app/src/components/UserCard.js
@@ -1,10 +1,21 @@
 import React from "react";
 
-const UserCard = () => {
-    return (
-        <div>
-        </div>
-    )
+class UserCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            followers: []
+        }
+    }
+
+    render() {
+        const {userData} = this.props;
+        console.log("props from usercard class", userData)
+        return (
+            <div>
+            </div>
+        )
+    }
 }
 
 export default UserCard;
