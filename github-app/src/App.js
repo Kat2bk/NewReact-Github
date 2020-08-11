1dcf9327388254fe4a265b8a1e43a22b5df1e6dd
unknown <Katherine2bk@gmail.com>
2020-08-11 13:15:58 -0700

    set up functional component for UserCard.js

--- github-app/src/App.js
+++ github-app/src/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import './App.css';
 import axios from "axios";
+import UserCard from "./components/UserCard";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      data: []
+      user: []
     }
   }
 
   componentDidMount() {
     axios.get("https://api.github.com/users/Kat2bk")
     .then(response => {
-      console.log("This is the data", response)
+      console.log("This is the data", response.data)
+      this.setState({ user: response.data})
     })
     .catch(error => {
-      console.log("this is the error", error)
+      console.log("Unable to grab data", error)
     })
   }
 
@@ -26,6 +28,7 @@ class App extends React.Component {
       <div className="App-container">
       <h1>GitHub Users</h1>
       <h2>Let's Git Crazy!</h2>
+      <UserCard />
       </div>
     )
   }
