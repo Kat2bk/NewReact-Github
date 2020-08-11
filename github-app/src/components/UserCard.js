c90a25fbc4d72c0ae55d1c4b50cf905ef7114e71
unknown <Katherine2bk@gmail.com>
2020-08-11 14:11:20 -0700

    finished setting up props in UserCard.js... tried to link an icon, started with router, ended up using a standard a href tag directed to props

--- github-app/src/components/UserCard.js
+++ github-app/src/components/UserCard.js
@@ -1,4 +1,6 @@
 import React from "react";
+import axios from "axios";
+import {DiGithubFull} from "react-icons/di";
 
 class UserCard extends React.Component {
     constructor(props) {
@@ -8,11 +10,28 @@ class UserCard extends React.Component {
         }
     }
 
+    componentDidMount() {
+        axios.get("https://api.github.com/users/Kat2bk/followers")
+        .then(response => {
+            console.log("data from followers api", response)
+        })
+        .catch(error => {
+            console.log("unable to access data", error)
+        })
+    }
+
     render() {
-        const {userData} = this.props;
-        console.log("props from usercard class", userData)
         return (
-            <div>
+            <div className="usercard-container">
+            <div className="usercard">
+            <img className="card-picture" src={this.props.userData.avatar_url}/>
+            <h2>{this.props.userData.name}</h2>
+            <h3>{this.props.userData.bio}</h3>
+            <a href={`click here: ${this.props.userData.html_url}`}><DiGithubFull /></a>
+            </div>
+            <div className="usercard-followers">
+            {}
+            </div>
             </div>
         )
     }
